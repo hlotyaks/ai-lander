b04e297de865220ce45c5c2d9ab8c632e90acc0c
hlotyaks <hlotyaks@gmail.com>
2025-06-12 17:37:23 -0400

    Refactor game structure by introducing Lander and LandingPad classes for improved object-oriented design and encapsulation of game state management.

---
+++ LanderGame/Lander.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Drawing;
+
+namespace LanderGame
+{
+    public class Lander
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float Vx { get; private set; }
+        public float Vy { get; private set; }
+        public float Angle { get; private set; }
+        public float Fuel { get; private set; }
+
+        private readonly float thrustPower = 0.002f;
+        private readonly float rotationSpeed = 0.005f;
+        private readonly float fuelConsumptionRate = 0.02f;
+
+        public Lander(float startX, float startY, float startFuel = 100f)
+        {
+            Reset(startX, startY, startFuel);
+        }
+
+        public void Reset(float startX, float startY, float startFuel = 100f)
+        {
+            X = startX;
+            Y = startY;
+            Vx = Vy = Angle = 0f;
+            Fuel = startFuel;
+        }
+
+        public void Update(float delta, bool thrusting, bool rotatingLeft, bool rotatingRight, float gravity)
+        {
+            if (rotatingLeft) Angle -= rotationSpeed * delta;
+            if (rotatingRight) Angle += rotationSpeed * delta;
+
+            if (thrusting && Fuel > 0f)
+            {
+                Vx += (float)Math.Sin(Angle) * thrustPower * delta;
+                Vy += -(float)Math.Cos(Angle) * thrustPower * delta;
+                Fuel -= fuelConsumptionRate * delta;
+                if (Fuel < 0f) Fuel = 0f;
+            }
+
+            Vy += gravity * delta;
+            X += Vx * delta;
+            Y += Vy * delta;
+        }
+
+        public void Draw(Graphics g, bool thrusting)
+        {
+            var worldXform = g.Transform;
+            g.TranslateTransform(X, Y);
+            g.RotateTransform(Angle * 180f / (float)Math.PI);
+            PointF[] shipTri = { new PointF(0, -20), new PointF(-10, 20), new PointF(10, 20) };
+            using var shipPen = new Pen(Color.White, 2);
+            g.DrawPolygon(shipPen, shipTri);
+            if (thrusting)
+            {
+                var rand = new Random();
+                PointF[] flame = { new PointF(-5, 20), new PointF(0, 20 + rand.Next(5, 15)), new PointF(5, 20) };
+                using var flamePen = new Pen(Color.Orange, 2);
+                g.DrawPolygon(flamePen, flame);
+            }
+            g.Transform = worldXform;
+        }
+    }
+}
